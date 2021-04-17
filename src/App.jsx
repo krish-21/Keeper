56524a576a586da3a1d9df822c949d6673c61490
Krishna Nanduri <krishna.21nv@gmail.com>
2021-04-17 11:31:27 +0530

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
 
+import notes from "./notes";
+
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map(note => (
+        <Note
+          key={note.key}
+          title={note.title}
+          content={note.content}
+        />
+      ))}
       <Footer />
     </>
   );
