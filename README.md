427db76d7eeb2b7357dac43577bbb939d9f920b9
Krishna Nanduri <krishna.21nv@gmail.com>
2021-05-03 19:52:02 +0530

    bugfix & deployment

---
+++ README.md
@@ -0,0 +1,13 @@
+# Keeper
+
+Your Life, Organized
+
+## Features
+
+- View all Notes
+- Add Notes
+- Delete Notes
+
+## Website
+
+Head to [Keeper](https://krish-21.github.io/Keeper/).
--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -3,28 +3,28 @@ import { useState } from "react";
 import { Add } from "@material-ui/icons";
 import { Zoom, Fab } from "@material-ui/core";
 
-const NoteForm = props => {
+const NoteForm = (props) => {
   const [isExpanded, setExpanded] = useState(false);
 
   const exapnd = () => {
     setExpanded(true);
-  }
+  };
 
   const [newNote, setNewNote] = useState({
     title: "",
     content: "",
   });
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
 
-    setNewNote(prevValue => ({
+    setNewNote((prevValue) => ({
       ...prevValue,
       [name]: value,
     }));
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     props.onSubmit(newNote);
     setNewNote({ title: "", content: "" });
     event.preventDefault();
@@ -32,13 +32,15 @@ const NoteForm = props => {
 
   return (
     <form className="create-note" onSubmit={handleSubmit}>
-      { isExpanded && <input
-        type="text"
-        name="title"
-        placeholder="Title"
-        onChange={handleChange}
-        value={newNote.title}
-      />}
+      {isExpanded && (
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={newNote.title}
+        />
+      )}
       <textarea
         name="content"
         placeholder="Take a Note"
@@ -48,7 +50,7 @@ const NoteForm = props => {
         value={newNote.content}
       />
       <Zoom in={isExpanded}>
-        <Fab>
+        <Fab onClick={handleSubmit}>
           <Add />
         </Fab>
       </Zoom>
